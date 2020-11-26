88def392a9460e10547dac45418660b847acaf2d
alicia-colom <aliciacolom@gmail.com>
2020-11-26 18:37:19 +0100

    Clean up and fix css styles in error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,16 +1,14 @@
 import '../stylesheets/Error.scss';
 import { Link } from 'react-router-dom';
-import Header from './Header';
-import Footer from './Footer';
 
 const CharacterDetail = (props) => {
 	return (
 		<>
 			<main className="error">
-				<Link to="/home" className="error__link">
-					Try again your search
-				</Link>
 				<div className="error__container">
+					<Link to="/home" className="error__container--link">
+						Try again your search
+					</Link>{' '}
 					<h1 className="error__container--title">
 						This character doesn't exist.
 					</h1>
@@ -19,11 +17,8 @@ const CharacterDetail = (props) => {
 					<h4>You made it up!!</h4>
 					<h5>Do you think you are better than us?!</h5>
 					<h6>What the *$%* !?</h6>
-
 				</div>
 			</main>
-
-			<Footer />
 		</>
 	);
 };
