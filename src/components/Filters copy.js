e3229f957cf07e740ccaf63f16a4cad4f0e53b0e
alicia-colom <aliciacolom@gmail.com>
2020-11-26 11:50:54 +0100

    Branch dev: copy origin filter from filters file into a new file

---
+++ src/components/Filters copy.js
@@ -0,0 +1,82 @@
+import '../stylesheets/Filters.scss';
+
+const Filters = (props) => {
+	const handleChangeStatus = (ev) => {
+		const statusValue = ev.target.value;
+		return props.handleFilterStatus(statusValue);
+	};
+
+	const handleClickGender = (ev) => {
+		console.log('filtro por gender', ev.target.value);
+	};
+
+	// Filtro para species
+	const specieList = props.resultList.map((eachCharacter) => (
+		<option key={eachCharacter.id} value={eachCharacter.species}>
+			{eachCharacter.species}
+		</option>
+	));
+
+	const handleChangeSpecie = (ev) => {
+		console.log('filtro por specie', ev.target.value);
+	};
+
+	return (
+		<form className="filter">
+			<fieldset className="filter__fieldset">
+				<label htmlFor="status">Status: </label>
+				<select
+					className="filter__select"
+					name="status"
+					id="status"
+					onChange={handleChangeStatus}
+				>
+					<option value="Alive">Alive</option>
+					<option value="Dead">Dead</option>
+					<option value="unknown">unknown</option>
+				</select>
+			</fieldset>
+
+			<fieldset className="filter__fieldset">
+				<label htmlFor="Female">Female</label>
+				<input
+					type="radio"
+					id="Female"
+					name="gender"
+					value="Female"
+					className="filter__gender"
+					onChange={handleClickGender}
+				/>
+				<label htmlFor="Male">Male</label>
+				<input
+					type="radio"
+					id="Male"
+					name="gender"
+					value="Male"
+					onChange={handleClickGender}
+				/>
+				<label htmlFor="unknown">unknown</label>
+				<input
+					type="radio"
+					id="unknown"
+					name="gender"
+					value="unknown"
+					onChange={handleClickGender}
+				/>
+			</fieldset>
+			<fieldset className="filter__fieldset">
+				<label htmlFor="specie">Species: </label>
+				<select
+					className="filter__select"
+					name="specie"
+					id="specie"
+					onChange={handleChangeSpecie}
+				>
+					{specieList}
+				</select>
+			</fieldset>
+		</form>
+	);
+};
+
+export default Filters;
